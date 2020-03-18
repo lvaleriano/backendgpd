1b3347dfd317478e8a117aece0293b13b483aa82
LUIS MIGUEL <lvaleriano.ing@gmail.com>
2020-03-18 12:44:16 -0500

    Agregando Botón

--- cartasplist/src/app.js
+++ cartasplist/src/app.js
@@ -10,36 +10,50 @@ var HelloWorldLayer = cc.Layer.extend({
         cc.spriteFrameCache.addSpriteFrames(res.gamble);
 
         var sprite = new cc.Sprite();
+
         sprite.initWithSpriteFrameName("2D.png");
-
-        //  CREACION DEL EFECTO DEL GIRO DE LA CARTA
-        var actionScaleTurn = new cc.ScaleTo(1, 0, 1);
-        sprite.runAction(actionScaleTurn);
-
         sprite.attr({
             x: size.width / 2,
             y: size.height / 2
         });
         this.addChild(sprite, 0);
 
+        var button = new ccui.Button(res.Button_png);
+        button.loadTextures();
+        button.x = size.width / 6;
+        button.y = size.height / 6;
+        button.setTouchEnabled(true);
+        button.addTouchEventListener(listener1, sprite);
+        this.addChild(button);
+
+        var listener1 = cc.EventListener.create({       //Creamos el evento o listener y el metodo
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: function (touch, event) { // Parametros
+
+                var locationInNode = sprite.convertToNodeSpace(touch.getLocation());
+                var s = sprite.getContentSize();
+                var rect = cc.rect(0, 0, s.width, s.height);
+                if (cc.rectContainsPoint(rect, locationInNode)) {
+                    //  CREACION DEL EFECTO DEL GIRO DE LA CARTA
+                    var actionScaleTurn = new cc.ScaleTo(1, 0, 1);
+                    sprite.runAction(actionScaleTurn);
+                    return true;
+                }
+                return false;
+            },
+            onTouchEnded: function (touch, event) {
+                sprite.setColor(cc.color.WHITE);
+            }
+        });
+        cc.eventManager.addListener(listener1, sprite); // Añadimos al controlador de eventos
+
+
+
+
         return true;
     }
 });
-
-//     var FlipXTest = HelloWorldLayer.extend({
-//     title:function () {
-//         return "FlipX3D";
-//     },
-//     code:function () {
-//         return "a = cc.flipX3D(duration )";
-//     },
-//     getEffect:function(duration) {
-//         var a = cc.flipX3D.create(2);
-//         var delay = cc.delayTime.create(2);
-//         var r = a.reverse();
-//         return cc.sequence.create( a, delay, r );
-//     }
-// });
 
 
 var HelloWorldScene = cc.Scene.extend({
--- cartasplist/src/resource.js
+++ cartasplist/src/resource.js
@@ -2,6 +2,7 @@ var res = {
     HelloWorld_png : "res/HelloWorld.png",
     gamble_2_png :"res/gamble_2.png",
     gamble : "res/gamble_2.plist",
+    Button_png: "res/Button.png",
 };
 
 var g_resources = [];
