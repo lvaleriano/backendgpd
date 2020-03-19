4ac39b7fad41a0ef2cef2fded49152dca94cd573
LUIS MIGUEL <lvaleriano.ing@gmail.com>
2020-03-19 18:12:12 -0500

    Funcionalidad del button

--- cartasplist/src/app.js
+++ cartasplist/src/app.js
@@ -1,16 +1,33 @@
-
+var sprite2;
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
+
+
     ctor:function () {
 
         this._super();
 
         var size = cc.winSize;
 
+
+        var button = new ccui.Button(res.Button_png);
+        button.loadTextures();
+        button.setTitleText("GIRAR");
+        button.setTitleColor(255, 125, 0, 0.7);
+        button.setTitleFontSize(16);
+        button.setTitleFontName("MarkerFelt.ttf");
+        button.x = size.width / 6;
+        button.y = size.height / 6;
+        button.addTouchEventListener(this.buttonListener.bind(this));
+        this.addChild(button);
+
+        return true;
+    },
+
+    buttonListener:function(sender,type)    {
+        var size = cc.winSize;
         cc.spriteFrameCache.addSpriteFrames(res.gamble);
-
         var sprite = new cc.Sprite();
-
         sprite.initWithSpriteFrameName("2D.png");
         sprite.attr({
             x: size.width / 2,
@@ -18,41 +35,11 @@ var HelloWorldLayer = cc.Layer.extend({
         });
         this.addChild(sprite, 0);
 
-        var button = new ccui.Button(res.Button_png);
-        button.loadTextures();
-        button.x = size.width / 6;
-        button.y = size.height / 6;
-        button.setTouchEnabled(true);
-        button.addTouchEventListener(listener1, sprite);
-        this.addChild(button);
-
-        var listener1 = cc.EventListener.create({       //Creamos el evento o listener y el metodo
-            event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            swallowTouches: true,
-            onTouchBegan: function (touch, event) { // Parametros
-
-                var locationInNode = sprite.convertToNodeSpace(touch.getLocation());
-                var s = sprite.getContentSize();
-                var rect = cc.rect(0, 0, s.width, s.height);
-                if (cc.rectContainsPoint(rect, locationInNode)) {
-                    //  CREACION DEL EFECTO DEL GIRO DE LA CARTA
-                    var actionScaleTurn = new cc.ScaleTo(1, 0, 1);
-                    sprite.runAction(actionScaleTurn);
-                    return true;
-                }
-                return false;
-            },
-            onTouchEnded: function (touch, event) {
-                sprite.setColor(cc.color.WHITE);
-            }
-        });
-        cc.eventManager.addListener(listener1, sprite); // Añadimos al controlador de eventos
-
-
-
-
-        return true;
+        var actionScaleTurn = new cc.ScaleTo(1, 0, 1);
+        sprite.runAction(actionScaleTurn);
     }
+
+
 });
 
 
